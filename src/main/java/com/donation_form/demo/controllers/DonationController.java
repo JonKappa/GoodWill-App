f9ad61c5c607fc8d69ade07ad0499eda9c9021fc
JonKappa <cappytwoj@gmail.com>
2021-11-17 16:07:14 -0500

    first

--- src/main/java/com/donation_form/demo/controllers/DonationController.java
+++ src/main/java/com/donation_form/demo/controllers/DonationController.java
@@ -1,14 +1,18 @@
 package com.donation_form.demo.controllers;
 
+import com.donation_form.demo.exceptions.DonationNotFoundException;
 import com.donation_form.demo.models.Donation;
 import com.donation_form.demo.services.DonationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DonationController {
@@ -22,13 +26,32 @@ public class DonationController {
     }
 
     @GetMapping("/{phoneNumber}")
-    public ResponseEntity<Donation> getDonationByPhoneNumber(@PathVariable String phoneNumber) {
-        return null;
+    public ResponseEntity<List<Donation>> getDonationByPhoneNumber(@PathVariable String phoneNumber) {
+        try {
+            List<Donation> donations = donationService.getByPhoneNumber(phoneNumber);
+            ResponseEntity<List<Donation>> response = new ResponseEntity<>(donations, HttpStatus.OK);
+            return response;
+        }
+        catch (DonationNotFoundException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Donation> getDonationById(@PathVariable Integer id) {
-        return null;
+        try {
+            Donation donation = donationService.getById(id);
+            ResponseEntity<Donation> response = new ResponseEntity<>(donation, HttpStatus.OK);
+            return response;
+        }
+        catch (DonationNotFoundException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+
     }
 
 
